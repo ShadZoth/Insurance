39fff1d06770a549d8f99eaa8a0f38baca86e9ad
ShadZoth <megazoth@gmail.com>
2014-07-11 17:01:20 +0400

    Company.representatives deleted

--- grails-app/domain/insurance/Company.groovy
+++ grails-app/domain/insurance/Company.groovy
@@ -4,9 +4,7 @@ class Company extends Client {
 
     String name
     String inn
-    static hasMany = {
-        representatives: Person
-    }
+
     static constraints = {
         name(nullable: false, maxSize: 30)
         inn(nullable: false, size: 10..12)
