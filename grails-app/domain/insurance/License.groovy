97f46b6ac961c874df3bdf7e5905b709b3461f5a
Mikhail Fomichev <mikhail.fomichev94@gmail.com>
2014-07-11 14:00:23 +0400

    License - Person connection

--- grails-app/domain/insurance/License.groovy
+++ grails-app/domain/insurance/License.groovy
@@ -3,12 +3,12 @@ package insurance
 class License {
 
     String number
-
     Date issueDate
-
     Date expirationDate
-
     int categories
+    Person owner
+
+    static belongsTo = Person
 
     static constraints = {
     }
