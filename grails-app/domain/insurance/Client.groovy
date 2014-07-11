10092316593f67fad5fd27e358ed03621342c7e2
ShadZoth <megazoth@gmail.com>
2014-07-11 17:14:21 +0400

    mapping of Client changed

--- grails-app/domain/insurance/Client.groovy
+++ grails-app/domain/insurance/Client.groovy
@@ -4,6 +4,10 @@ class Client {
     Date registrationDate
     Boolean archived
 
+    static mapping = {
+        tablePerHierarchy(false)
+    }
+
     static hasMany = [vehicles: Vehicle, contacts: Contact, warrants: Warrant]
 
     static constraints = {
