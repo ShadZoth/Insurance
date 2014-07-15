e24471cfebbe977bda97a59e550d3b0c6dbb5380
ShadZoth <megazoth@gmail.com>
2014-07-15 18:27:18 +0400

    Certificate.issueDate constraint

--- grails-app/domain/insurance/Certificate.groovy
+++ grails-app/domain/insurance/Certificate.groovy
@@ -12,16 +12,32 @@ class Certificate {
 
     static belongsTo = Vehicle
 
+    static mapping = {
+        issueDate sqlType: "date"
+    }
+
     static constraints = {
         number(nullable: false)
         vehicle()
         color(maxSize: 18, nullable: true)
-        issueDate(nullable: false)
+        issueDate(nullable: false, shared: 'upToDate')
+//                validator: {val, obj ->
+//                    moreThanLast(val, obj) && upToDate(val)
+//                })
         ownerFirstName(nullable: false, maxSize: 30)
         ownerLastName(nullable: false, maxSize: 30)
         vin(maxSize: 17)
     }
 
+//    static def upToDate(Date val) {
+//        val <= new Date()
+//    }
+//
+//    static def moreThanLast(Date val, Certificate obj) {
+//        if (obj.vehicle.certificates) {
+//            val > obj.vehicle.certificates.issueDate.max()
+//        } else true
+//    }
 
     @Override
     public String toString() {
