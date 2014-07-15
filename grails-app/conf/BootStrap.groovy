1a7376bdcf716e5c145aac1b2046a2da9fdbf814
samovar242 <samovar242@yandex.ru>
2014-07-15 16:19:31 +0400

    Secure. Начало. Be careful

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,7 @@
 import insurance.Client
+import insurance.Role
+import insurance.User
+import insurance.UserRole
 
 class BootStrap {
 
@@ -7,7 +10,20 @@ class BootStrap {
         c.registrationDate = new Date()
         c.archived = true
         c.save()
+
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        def testUser = new User(username: 'me', password: 'password')
+        testUser.save(flush: true)
+
+        UserRole.create testUser, adminRole, true
+
+        assert User.count() == 1
+        assert Role.count() == 2
+        assert UserRole.count() == 1
     }
     def destroy = {
+
     }
 }
