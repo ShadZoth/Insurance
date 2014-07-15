614f37b7f4cd2ba47272239ca94e3077aa2869ec
ShadZoth <megazoth@gmail.com>
2014-07-15 17:05:18 +0400

    Price.since constraint

--- grails-app/domain/insurance/Price.groovy
+++ grails-app/domain/insurance/Price.groovy
@@ -7,7 +7,13 @@ class Price {
     Product product
     static constraints = {
         product()
-        since()
+        since validator: { val, obj ->
+            def lil = new Date(Calendar.getInstance().get(Calendar.YEAR) - 1900,
+                    Calendar.getInstance().get(Calendar.MONTH),
+                    Calendar.getInstance().get(Calendar.DAY_OF_MONTH))
+            def res = val >= lil
+            return res
+        }
         value() // TODO: в БД определить как money
     }
 
