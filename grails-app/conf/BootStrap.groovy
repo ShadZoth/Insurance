bb536942bb5f12a1f545720ad1dfa3ece9c7a126
Iskakov Ildar <samovar242@yandex.ru>
2014-07-16 11:31:00 +0400

    добавлены все роли

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,7 +12,9 @@ class BootStrap {
         c.save()
 
         def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+        def managerRole = new Role(authority: 'ROLE_MANAGER').save(flush: true)
+        def sellerRole = new Role(authority: 'ROLE_SELLER').save(flush: true)
+        def callCenterRole = new Role(authority: 'ROLE_CALL_CENTER').save(flush: true)
 
         def testUser = new User(username: 'me', password: 'password')
         testUser.save(flush: true)
