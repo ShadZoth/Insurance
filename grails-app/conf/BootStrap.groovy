3c045137aabc522aaf6a542ff04c0ff16cc63bd5
gorsvet <linuxedhorse@gmail.com>
2014-07-16 13:43:06 +0400

    Созданы все пользователи.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -16,21 +16,36 @@ class BootStrap {
         def sellerRole = new Role(authority: 'ROLE_SELLER').save(flush: true)
         def callCenterRole = new Role(authority: 'ROLE_CALL_CENTER').save(flush: true)
 
-        def testUser = new User(username: 'me', password: 'password')
+        def testUser = new User(username: 'admin', password: 'password')
         testUser.save(flush: true)
         testUser.enabled = true
 
-        def testSeller = new User(username: 'he', password: 'password')
+        UserRole.create testUser, adminRole, true
+
+
+        def testManager = new User(username: 'manager', password: 'password')
+        testManager.save(flush: true)
+        testManager.enabled = true
+
+        UserRole.create testManager, managerRole, true
+
+        def testSeller = new User(username: 'seller', password: 'password')
         testSeller.save(flush: true)
         testSeller.enabled = true
 
-        UserRole.create testUser, adminRole, true
         UserRole.create testSeller, sellerRole, true
 
-        assert User.count() == 2
+        def testCallCenter = new User(username: 'call', password: 'password')
+        testCallCenter.save(flush: true)
+        testCallCenter.enabled = true
+
+        UserRole.create testCallCenter, callCenterRole, true
+
+        assert User.count() == 4
         assert Role.count() == 4
-        assert UserRole.count() == 2
+        assert UserRole.count() == 4
     }
+
     def destroy = {
 
     }
