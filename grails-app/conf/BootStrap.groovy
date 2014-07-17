379ee24472dcdd06a96998b2859c45cfb535f4e3
ShadZoth <megazoth@gmail.com>
2014-07-17 12:28:54 +0400

    2 managers

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,5 @@
 import insurance.Role
 import insurance.User
-import insurance.UserRole
 
 class BootStrap {
 
@@ -10,24 +9,21 @@ class BootStrap {
         def sellerRole = new Role(authority: 'ROLE_SELLER').save(flush: true)
         def callCenterRole = new Role(authority: 'ROLE_CALL_CENTER').save(flush: true)
 
-        def testUser = new User(username: 'me', password: 'password', authority: 'ROLE_ADMIN')
+        def testUser = new User(username: 'me',
+                password: 'password',
+                authority: 'ROLE_ADMIN')
         testUser.save(flush: true)
         testUser.mergeAuthorities()
         testUser.enabled = true
 
-        def testSeller = new User(username: 'he', password: 'password', authority: 'ROLE_SELLER')
-        testSeller.save(flush: true)
-        testSeller.mergeAuthorities()
-        testSeller.enabled = true
-
-        def testManager = new User(username: 'she', password: 'password', authority: 'ROLE_MANAGER')
-        testManager.save(flush: true)
-        testManager.mergeAuthorities()
-        testManager.enabled = true
-
-        assert User.count() == 3
-        assert Role.count() == 4
-        assert UserRole.count() == 3
+        (1..2).collect {
+            new User(username: "manager$it",
+                    password: 'password',
+                    authority: 'ROLE_MANAGER')
+        }.each {
+            it.save(flush: true)
+            it.mergeAuthorities()
+        }
     }
     def destroy = {
 
