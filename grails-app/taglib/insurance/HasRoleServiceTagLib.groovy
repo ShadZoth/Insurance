b2f6854fe2f9f80f6a0736a8399799d9990dc336
gorsvet <linuxedhorse@gmail.com>
2014-07-22 11:49:20 +0400

    Комментирован таглиб.

--- grails-app/taglib/insurance/HasRoleServiceTagLib.groovy
+++ grails-app/taglib/insurance/HasRoleServiceTagLib.groovy
@@ -6,11 +6,13 @@ class HasRoleServiceTagLib {
 
     def hasRoleService
     def usersHavingRole = { attrs ->
-       out<< g.select([id: attrs.id, name: attrs.name, from:
+       out << g.select([id: attrs.id, name: attrs.name, from:
                User.list().findAll {
+                   //Если требуется только текущей пользователь
                    if (attrs.onlyMe) {
                        def me = User.findByUsername(SecurityContextHolder.getContext().getAuthentication().getPrincipal().username)
                        return it == me
+                   // Если требуется пользователь с данной ролью
                    } else {
                        return hasRoleService.serviceMethod(it, attrs.role)
                    }
