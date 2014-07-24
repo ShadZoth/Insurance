93233fb1a3d3ddf0ba55ac281bea039155b82b2f
ShadZoth <megazoth@gmail.com>
2014-07-24 13:04:46 +0400

    public Sex

--- grails-app/domain/insurance/Passport.groovy
+++ grails-app/domain/insurance/Passport.groovy
@@ -29,7 +29,7 @@ class Passport implements Comparable<Passport> {
         })
     }
 
-    private static enum Sex {
+    static enum Sex {
         MALE("Мужской"), FEMALE("Женский")
 
         Sex(String s) {
